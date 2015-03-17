9ed716fe02e07930428d82f9f11d2379462b7e53
Lindan <wudaoxiao@126.com>
2015-03-17 14:06:49 +0800

    Updated  添加说明

---
+++ script/urls.js
@@ -0,0 +1,49 @@
+var urls = {
+    'ued': [
+        'http://cued.xunlei.com/feed',//迅雷CUED
+        "http://tid.tenpay.com/?feed=rss2",//腾讯财付通TID
+        'http://www.alloyteam.com/?feed=rss2',//腾讯Web前端 Alloy 团队
+        //"http://feed.feedsky.com/wsdued",//腾讯移动互联网设计WSD
+
+        'http://isux.tencent.com/feed',//腾讯ISUX
+        'http://cdc.tencent.com/?feed=rss2',//腾讯CDC
+        "http://uxc.360.cn/feed",//360UXC
+        "http://www.75team.com/feed",//奇舞团
+        "http://mux.alimama.com/?feed=rss2",//阿里妈妈MUX
+        "http://mued.sohu.com/feed/",//搜狐MUED搜狐MUED
+        "http://ued.focus.cn/wordpress/?feed=rss2",//搜狐焦点UED
+        "http://ued.ctrip.com/blog/?feed=rss2",//携程UED
+        "http://udc.weibo.com/feed/",//微博UDC
+        "http://ued.sina.com.cn/?feed=rss2",//新浪UED
+        "http://www.aliued.cn/feed",//阿里巴巴（中国站）用户体验设计部
+        "http://kux.kingdee.com/?feed=rss2",//"有意思"的用户体验团队
+        "http://ued.baidu.com/?feed=rss2",//百度搜索用户体验中心
+        "http://mux.baidu.com/?feed=rss2",//百度移动用户体验部
+        "http://uedc.163.com/feed",//网易用户体验设计中心
+        "http://mweb.baidu.com/feed",//MWEB 百度移动云前端
+        "http://qqfe.org/feed/",//腾讯网前端团队
+        "http://gux.163.com/feed",//网易GUX游戏用户体验中心
+        "http://fex.baidu.com/feed.xml",//FEX 百度 Web 前端研发部
+        "http://jdc.jd.com/feed",//京东设计中心
+
+        //'http://www.wxwdesign.com/feed',
+        'http://forcefront.com/feed/',
+        'http://www.css88.com/feed',
+        'http://mweb.baidu.com/?feed=rss2',
+        'http://www.qianduan.net/feed',
+        //'http://sofish.de/feed',
+        'http://www.zhangxinxu.com/wordpress/feed/',
+        'http://fex.baidu.com/feed.xml',
+        'http://feed.cnblogs.com/blog/u/101461/rss',
+        'http://www.itzhai.com/feed',
+
+        'http://blog.19ued.com/?feed=rss2',
+        'http://www.36ria.com/feed/'
+    ],
+
+    'seo': [
+        'http://www.semyj.com/feed',
+        'http://blog.sina.com.cn/rss/2125736794.xml',
+        'http://www.seozac.com/feed/'
+    ]
+};
